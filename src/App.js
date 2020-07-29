9d89b297becd0ca8b7621b6f75239992c6f3cf18
Luis Sousa <lv-ss@hotmail.com>
2020-07-29 15:58:24 -0300

    segunda aula WIP

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import Header from './components/header';
+import styled from 'styled-components'
+
+const AppWrapper = styled.div`
+  background: var(--grayDark);
+
+  padding-top: 94px;
+
+  @media(max-width: 800px){
+    padding-top: 40px;
+  }
+`;
 
 function App() {
   return (
-    <div>
+    <AppWrapper>
       <Header/>
-    </div>
+    </AppWrapper>
   );
 }
 
---
+++ src/components/BannerMain/styles.js
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+
+export const BannerMainContainer = styled.section`
+  height: 80vh;
+  color: var(--white);
+  background-image: ${({backgroundImage}) => `url(${backgroundImage})`};
+`;
+
+export const ContentAreaContainer = styled.section`
+
+`;
+
+ContentAreaContainer.Item = styled.div`
+
+`;
+
+ContentAreaContainer.Category = styled.h1`
+
+`;
+
+ContentAreaContainer.Description = styled.p`
+
+`;
+
+ContentAreaContainer.Title = styled.h2`
+
+`;
+
+
+export const WatchButton = styled.button`
+
+`;
