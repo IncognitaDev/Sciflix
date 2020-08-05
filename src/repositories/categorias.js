126f764b5eec4e0d93199bb9d8e90c9431c588c9
Luis Sousa <lv-ss@hotmail.com>
2020-08-05 05:22:03 -0300

    :construction:  Adicionando criação de categoria

--- src/repositories/categorias.js
+++ src/repositories/categorias.js
@@ -2,6 +2,20 @@ import config from '../config';
 
 const URL_CATEGORIES = `${config.URL}/categorias`;
 
+function create(obj){
+  return fetch(`${URL_CATEGORIES}`, {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json'
+    },
+    body: JSON.stringify(obj)
+  }).then(async (response) => {
+    const resposta = await response.json()
+
+    return resposta
+  } )
+}
+
 function getAll() {
   return fetch(`${URL_CATEGORIES}`).then(async (response) => {
     const resposta = await response.json();
@@ -19,4 +33,5 @@ function getAll() {
 export default {
   getAllWithVideos,
   getAll,
+  create,
 }
